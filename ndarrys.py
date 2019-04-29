04487754df8a1705ab99854a57d479c9604a9206
andrew.holt <ajh1143@uw.edu>
2019-04-29 15:10:54 -0700

    Added function to calculate and print shape of ndarry parameter

--- ndarrys.py
+++ ndarrys.py
@@ -1,1 +1,11 @@
 import numpy as np
+
+def find_shape(arr):
+    """
+    Find and print the shape of given ndarray parameter
+    :param arr: Multidimensional array object
+    :return: None
+    """
+    show_shape = arr.shape
+    print(f"This array is of shape: {show_shape}")
+    return None
