66705dbce853766e675972e74d9ae4b2bb9193a5
andrew.holt <ajh1143@uw.edu>
2019-04-29 15:15:43 -0700

    Added function to generate a NumPy ndarray from list of values

--- ndarrys.py
+++ ndarrys.py
@@ -1,5 +1,15 @@
 import numpy as np
 
+def create_ndarray(values):
+    """
+    Generate and return and ndarray
+    :param values: List of values to be added to ndarray
+    :return: NumPy Multidimensional array
+    """
+    generated_ndarray = np.array(values)
+    return generated_ndarray
+
+
 def find_shape(arr):
     """
     Find and print the shape of given ndarray parameter
@@ -9,3 +19,4 @@ def find_shape(arr):
     show_shape = arr.shape
     print(f"This array is of shape: {show_shape}")
     return None
+
