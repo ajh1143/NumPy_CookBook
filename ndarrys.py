ccada05ece681baf0d25d5a9be62d673d9fe54e4
andrew.holt <ajh1143@uw.edu>
2019-04-29 15:30:24 -0700

    Added functions to generate vectorized NumPy ndarray via addition, subtraction, multiplication and exponentiation.

--- ndarrys.py
+++ ndarrys.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+
 def create_ndarray(values):
     """
     Generate and return and ndarray
@@ -20,3 +21,40 @@ def find_shape(arr):
     print(f"This array is of shape: {show_shape}")
     return None
 
+
+def vectorize_add(arr1, arr2):
+    """
+    :param arr1: NumPy Array 1
+    :param arr2: NumPy Array 2
+    :return: Vectorized ndarray comprised of the addition of Array 1 and Array 2
+    """
+    result = arr1+arr2
+    return result
+
+
+def vectorize_subtract(arr1, arr2):
+    """
+    :param arr1: NumPy Array 1
+    :param arr2: NumPy Array 2
+    :return: Vectorized ndarray comprised of the addition of Array 1 and Array 2
+    """
+    result = arr1-arr2
+    return result
+
+def vectorize_multiply(arr1, arr2):
+    """
+    :param arr1: NumPy Array 1
+    :param arr2: NumPy Array 2
+    :return: Vectorized ndarray comprised of the multiplaction of Array 1 and Array 2
+    """
+    result = arr1*arr2
+    return result
+
+def vectorize_exponential(arr1, value):
+    """
+    :param arr1: NumPy Array 1
+    :param value: integer or float
+    :return: Vectorized ndarray comprised of the exponentiation of Array 1 and parameter value
+    """
+    result = arr1**value
+    return result
